0e66c380f2dd5e8d597781cc200774156355e527
Aundrei Bryant <drei97@gmail.com>
2022-12-08 12:50:52 -0600

    if statement review

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
+const username = "joewoe";
+const firstName = "Joe";
+const lastName = "Woe";
 
+const product1 = "t-shirt";
+const product2 = "socks";
+
+const total = 102;
+const discount = 0.1;
+
+if (total > 100) {
+  console.log(`you get a a discount${discount * 100}% discount`);
+  console.log(`Your total is now${total * (1 - discount)}`);
+} else {
+  console.log(`Your total is $${total}`);
+}
